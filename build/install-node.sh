68c2bbe898a99aa5063e6d2df65edcd820905309
Ahmed Haider <ahmed.haider@get10up.com>
2023-10-10 16:19:20 +0500

    19362336 | fix: npm - lightouse latest version compat fixed

--- build/install-node.sh
+++ build/install-node.sh
@@ -11,6 +11,8 @@
 # catch Errors
 set -euo pipefail
 
+# Updated install-node.sh due to the following issue, fixed in a more specific LTS version of 16 (v16.20.2 - gallium):
+# This strategy targets highest LTS for this release, not a speciifc version
 NODE_VERSION="16"
 
 # set up nvm in this script
@@ -21,12 +23,13 @@ echo "Building node environment for version ${NODE_VERSION}"
 nvm install "${NODE_VERSION}"
 
 # Cypress version added since latest supports v18.x and above. Setting Cypress version to 13.1.0 according to cypress changelog for supporting node v16.x releases.
+# Lighthouse breaking changes for [v11.x / latest](https://github.com/GoogleChrome/lighthouse/releases/tag/v11.0.0), release tag for Aug 4, 2023. Setting latest stable version for node 16 support. (~10.4)
 npm install -g \
     grunt-cli \
     gulp-cli \
     bower \
     yarn \
-    lighthouse \
+    lighthouse@~10.4 \
     serverless \
     firebase-tools \
     cypress@~13.1
